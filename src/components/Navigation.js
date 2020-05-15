d3d39575901b99d0b0b29cedd79bce167a5267cf
Jiayi Ren <jiayiren22@gmail.com>
2020-05-14 21:45:50 -0700

    add navigation with styles and transition, create Home component, evolution reducer

---
+++ src/components/Navigation.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Navigation = props =>{
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        console.log(document.documentElement.scrollTop)
+        const scroll = document.documentElement.scrollTop
+        const navBar = document.querySelector(".nav-bar")
+        const navItem = document.querySelectorAll(".nav-item")
+        if(scroll > 100){
+            navBar.style.position = "fixed"
+            navBar.style.top = "0"
+            navBar.style.backgroundColor = "#000000"
+            navBar.style.height = "50px"
+            navBar.style.margin = "0"
+            navItem[0].style.position = "fixed"
+            navItem[0].style.top = "18px"
+            navItem[0].style.left = "65vw"
+            navItem[1].style.position = "fixed"
+            navItem[1].style.top = "18px"
+            navItem[1].style.left = "80vw"
+        }else{
+            navBar.style.position = "static"
+            navBar.style.top = "0"
+            navBar.style.height = "50px"
+            navBar.style.margin = "20px 0"
+            navBar.style.backgroundColor = "#ffffff"
+            navItem[0].style.position = "static"
+            navItem[0].style.top = "0"
+            navItem[0].style.left = "0"
+            navItem[1].style.position = "static"
+            navItem[1].style.top = "0"
+            navItem[1].style.left = "0"
+        }
+    }
+
+    return (
+        <div className="nav">
+            <div className="nav-title">
+                <a href="/"><h1>P<div className="nav-pokeball"></div>kédex</h1></a>
+                <div className="nav-anime"></div>
+            </div>
+            <div className="nav-bar">
+                <nav>
+                    <ul>
+                        <li className="nav-item"><NavLink className="nav-link" to="/pokemon" activeClassName="selected">Pokémon</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="/games" activeClassName="selected">Games</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
@@ -10,13 +11,22 @@ import rootReducer from "./store/reducers";
 import './sass/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+
+
+    <Router>
+      <Navigation />
+      <Route exact path="/pokemon" component={App}/>
+      <Route path="/" component={Home}></Route>
+
+    </Router>
   </Provider>,
   rootElement
 );
