f12d83892bb7775ca95dd0fdf1b8e102e98d6f04
jiayi-ren <jiayiren95@gmail.com>
2020-10-27 22:38:24 -0700

    fix pagination issue

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from "./store/reducers";
 
-// import './index.css';
 import './sass/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 const AppWithRouter = withRouter(App);
 
 const rootElement = document.getElementById("root");
@@ -25,7 +22,3 @@ ReactDOM.render(
   rootElement
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
